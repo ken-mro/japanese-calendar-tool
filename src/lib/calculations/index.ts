404aa0667ff0df0763492cab609c78677f3bccc9
KenMurao <murao.ken.36x@kyoto-u.jp>
2026-01-12 21:33:10 +0900

    chore: export new calculation modules from index

--- src/lib/calculations/index.ts
+++ src/lib/calculations/index.ts
@@ -1,8 +1,18 @@
 // Main calculation index - exports all calculation functions
 
 export { getJapaneseEra, formatJapaneseEra, getEraList, convertEraToWesternYear, type JapaneseEra } from './japaneseEra';
-export { getChineseZodiac, type ChineseZodiac } from './zodiac';
+export { 
+  getChineseZodiac, 
+  getMonthZodiac, 
+  getDayZodiac, 
+  type ChineseZodiac, 
+  HEAVENLY_STEMS, // Export constants if needed by UI
+  EARTHLY_BRANCHES 
+} from './zodiac';
 export { getZodiacSign, type ZodiacSign } from './constellation';
-export { getNineStar, type NineStar } from './nineStar';
+export { getNineStar, getMonthNineStar, getDayNineStar, type NineStar } from './nineStar';
 export { getRokuyo, type Rokuyo } from './rokuyo';
 export { getMoonPhase, type MoonPhase } from './moon';
+export { getWafuGetsumei, type WafuGetsumei } from './wafuGetsumei';
+export { getJuniChoku, type JuniChoku } from './junichoku';
+
